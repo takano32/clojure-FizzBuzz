20728c8b4519fc14fd0b297372c5de8a6c40c3e8
TAKANO Mitsuhiro <takano32@gmail.com>
2012-03-27 20:44:15 +0900

    impl fb

--- fb.clj
+++ fb.clj
@@ -4,4 +4,20 @@
   (if (= 0 (mod x n))
     :true))
 
+(defn fb [n]
+  (if (= 0 n)
+    ()
+    (do
+      (fb  (- n 1))
+      (cond 
+        (divisible n 15) (println "FizzBuzz")
+        (divisible n 3 ) (println "Fizz")
+        (divisible n 5 ) (println "Buzz")
+        true (println n)
+        )
+      )
+  ))
+
+(fb 100)
+(println "")
 
